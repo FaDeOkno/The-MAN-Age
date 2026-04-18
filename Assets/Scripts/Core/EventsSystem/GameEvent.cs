260113a83373a9744c9d64dc2a08c2b07dae032e
_kote <logkedr18@gmail.com>
2026-04-18 19:09:10 +0400

    add: Event system

---
+++ Assets/Scripts/Core/EventsSystem/GameEvent.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Game Event")]
+public class GameEvent : ScriptableObject
+{
+    private readonly System.Collections.Generic.List<GameEventListener> listeners =
+        new System.Collections.Generic.List<GameEventListener>();
+
+    public void Raise(Component sender, object eventData = null)
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+            listeners[i].OnEventRaised(sender, eventData);
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+            listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (listeners.Contains(listener))
+            listeners.Remove(listener);
+    }
+}
+
+public class CustomGameEvent : UnityEvent<Component, object>
+{
+}
