d3cb958acedab7b06f6fe34ef7a29c4b51716166
_kote <logkedr18@gmail.com>
2026-04-20 00:55:41 +0400

    add: Hand visuals manager script for better animations

---
+++ Assets/Scripts/Core/Game/HandVisualsManager.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+public class HandVisualsManager : MonoBehaviour
+{
+    public static HandVisualsManager Instance { get; private set; }
+
+    [SerializeField] private Animator _gettingIdHand;
+    [SerializeField] private Animator _showingIdHand;
+    [SerializeField] private Animator _showingNewsHand;
+
+    private HandAnimation _currentAnimation = HandAnimation.None;
+    private bool _awaitingAnimationFinish = false;
+    private bool _hasId = false;
+
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnGetId(Component sender, object data)
+    {
+        PlayAnimation(HandAnimation.GettingId);
+    }
+
+    public void OnOpenNews(Component sender, object data)
+    {
+        PlayAnimation(HandAnimation.ShowingNews);
+    }
+
+    public void OnCloseNews(Component sender, object data)
+    {
+        PlayAnimation(_hasId ? HandAnimation.ShowingId : HandAnimation.None);
+    }
+
+    public void OnVisitorChecked(Component sender, object data)
+    {
+        _hasId = false;
+        PlayAnimation(HandAnimation.None);
+    }
+
+    public void PlayAnimation(HandAnimation animation)
+    {
+        if (_currentAnimation == animation)
+            return;
+
+        if (_awaitingAnimationFinish)
+            return;
+
+        switch (_currentAnimation)
+        {
+            case HandAnimation.ShowingId:
+                _showingIdHand.SetBool("IsActive", false);
+                break;
+            case HandAnimation.ShowingNews:
+                _showingNewsHand.SetBool("IsActive", false);
+                break;
+            default:
+                break;
+        }
+
+        switch (animation)
+        {
+            case HandAnimation.GettingId:
+                _gettingIdHand.Play("GetId");
+                break;
+            case HandAnimation.ShowingId:
+                _showingIdHand.SetBool("IsActive", true);
+                break;
+            case HandAnimation.ShowingNews:
+                _showingNewsHand.SetBool("IsActive", true);
+                break;
+            default:
+                break;
+        }
+
+        _currentAnimation = animation;
+        _awaitingAnimationFinish = true;
+    }
+
+    public void OnAnimationFinished()
+    {
+        _awaitingAnimationFinish = false;
+
+        if (_currentAnimation == HandAnimation.GettingId)
+        {
+            Debug.Log("Got ID");
+            _hasId = true;
+            PlayAnimation(HandAnimation.ShowingId);
+        }
+    }
+}
+
+public enum HandAnimation
+{
+    None,
+    GettingId,
+    ShowingId,
+    ShowingNews
+}
